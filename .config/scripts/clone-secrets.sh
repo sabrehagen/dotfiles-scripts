7933f8287b72dc9edaa56b229171f2087f6736ca
Jackson Delahunt <jackson@stemn.com>
2019-02-19 20:19:33 +1100

    ssh

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -1,5 +1,5 @@
 # Clone private ssh keys using password over https
-vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ssh
+vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ssh-private
 
 # Clone secrets using private key over ssh
 vcsh clone git@github.com:sabrehagen/dotfiles-stemn.git
