14e1a60643d4d0b8e118f94a53c581623ae2e31c
Jackson Delahunt <jackson@stemn.com>
2019-02-20 14:43:38 +1100

    clean

---
+++ .config/scripts/clean-secrets.sh
@@ -0,0 +1,6 @@
+# Remove vcsh repositories
+rm -rf ~/.config/vcsh/repo.d/dotfiles-ssh*
+
+# Remote secrets files
+rm -rf ~/.ssh
+rm -rf ~/.stemn.conf
--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -2,5 +2,8 @@
 vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ssh-private && \
   chmod 600 ~/.ssh/id_rsa
 
+# Unlock ssh private key so remaining repositories can be cloned
+keychain --eval id_rsa
+
 # Clone secrets using private key over ssh
 vcsh clone git@github.com:sabrehagen/dotfiles-stemn.git
