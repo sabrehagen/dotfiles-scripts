118795dd8b37a254e714ccf548ced536e2c181ff
Jackson Delahunt <jackson@stemn.com>
2019-02-20 15:12:45 +1100

    fix

--- .config/scripts/clean-secrets.sh
+++ .config/scripts/clean-secrets.sh
@@ -1,7 +1,7 @@
+# Remove vcsh repository contents
+vcsh dotfiles-ssh-private ls-files | xargs rm -f
+vcsh dotfiles-stemn ls-files | xargs rm -f
+
 # Remove vcsh repositories
 rm -rf ~/.config/vcsh/repo.d/dotfiles-ssh*
 rm -rf ~/.config/vcsh/repo.d/dotfiles-stemn
-
-# Remote secrets files
-rm -rf ~/.ssh
-rm -rf ~/.stemn.conf
