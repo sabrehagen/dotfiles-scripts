d500de857333bc6f35d00be7c2c9ef32a9984727
Jackson Delahunt <jackson@stemn.com>
2019-02-23 00:09:28 +1100

    startup

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -12,3 +12,9 @@ numlockx on
 
 # Set keyboard repeat delay and rate
 xset r rate 200 80
+
+# Manually start services whilst s6 issue persists
+tmux new-session -d -s desktop-environment
+tmux new-session -d -s gotty-clients
+tmux new-session -d -s gotty-server gotty --permit-write --port 8022 zsh -c 'tmux new-session -s gotty-clients-$(date +%s) -t gotty-clients'
+tmux new-session -d -s webrelay relay connect --name $HOSTNAME localhost:8022
