c1d32d9888980ca7196df014678225372407214f
Jackson Delahunt <jackson@stemn.com>
2019-02-23 14:10:00 +1100

    secreeets

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -1,10 +1,11 @@
-# Clone private ssh keys using password over https
-vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ssh-private && \
-  chmod 600 ~/.ssh/id_rsa
+# Clone private ssh keys using password over https if an ssh key is not already present
+if [ ! -f ~/.ssh/id_rsa ]; then
+  vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ssh-private && \
+    chmod 600 ~/.ssh/id_rsa
+fi
 
 # Unlock ssh private key so remaining repositories can be cloned
 keychain --eval id_rsa
 
 # Clone private repositories using ssh key
-vcsh clone git@github.com:sabrehagen/dotfiles-env-secrets.git
-vcsh clone git@github.com:sabrehagen/dotfiles-stemn.git
+vcsh clone git@github.com:sabrehagen/dotfiles-secrets.git
