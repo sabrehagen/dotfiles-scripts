29ac716ef42774dad3eb9e39defb26b6c7d6ff3e
Jackson Delahunt <jackson@stemn.com>
2019-02-24 17:24:27 +1100

    fix

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -1,7 +1,12 @@
+# Store private dotfiles separately from public ones
+PUBLIC_VCSH_REPOS=$HOME/.config/vcsh/repo.d
+PRIVATE_VCSH_REPOS=$HOME/.config/vcsh/repo-private.d
+export VCSH_REPO_D=$PRIVATE_VCSH_REPOS
+
 # Clone private ssh keys using password over https if an ssh key is not already present
-if [ ! -f ~/.ssh/id_rsa ]; then
+if [ ! -f $HOME/.ssh/id_rsa ]; then
   vcsh clone https://sabrehagen@github.com/sabrehagen/dotfiles-ssh-private && \
-    chmod 600 ~/.ssh/id_rsa
+    chmod 600 $HOME/.ssh/id_rsa
 fi
 
 # Unlock ssh private key so remaining repositories can be cloned
@@ -12,7 +17,6 @@ vcsh clone git@github.com:sabrehagen/dotfiles-notes.git
 vcsh clone git@github.com:sabrehagen/dotfiles-secrets.git
 
 # Convert all https cloned repositories to use ssh
-VCSH_REPOS=~/.config/vcsh/repo.d
-for REPOSITORY in $(ls $VCSH_REPOS); do
-  https-to-git $VCSH_REPOS/$REPOSITORY/config
+for REPOSITORY in $(ls -d $PUBLIC_VCSH_REPOS/* $PRIVATE_VCSH_REPOS/*); do
+  https-to-git $REPOSITORY/config
 done
