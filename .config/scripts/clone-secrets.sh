ca8202e07ae7b65ae16191ef6b7297e1d75b0262
Jackson Delahunt <jackson@stemn.com>
2019-02-25 17:37:50 +1100

    routine

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -16,6 +16,7 @@ fi
 keychain --eval id_rsa
 
 # Clone private repositories using ssh key
+vcsh clone git@github.com:sabrehagen/dotfiles-cloudflare.git &
 vcsh clone git@github.com:sabrehagen/dotfiles-notes.git &
 vcsh clone git@github.com:sabrehagen/dotfiles-secrets.git &
 
