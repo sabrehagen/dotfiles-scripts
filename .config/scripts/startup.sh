41d6f2752c454cb2b58cc849ee69ad1d7dbfc494
Jackson <jackson@stemn.com>
2019-03-02 03:44:23 +0000

    transmission

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -20,6 +20,16 @@ tmux new-session -d -s gotty-clients 2>/dev/null
 tmux new-session -d -s gotty-server \
   gotty --permit-write --port 8022 zsh -c 'tmux new-session -s gotty-clients-$(date +%s) -t gotty-clients' 2>/dev/null
 
+tmux new-session -d -s transmission \
+  transmission-daemon \
+  --bind-address-ipv4 localhost \
+  --config-dir $HOME/.config/transmission \
+  --download-dir $HOME/torrents \
+  --foreground \
+  --no-auth \
+  --rpc-bind-address localhost \
+  --watch-dir $HOME/torrents/.watch
+
 # Always restart webrelay to pick up new environment variables
 tmux set-environment -g RELAY_KEY $RELAY_KEY
 tmux set-environment -g RELAY_SECRET $RELAY_SECRET
