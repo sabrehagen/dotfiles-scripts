5301baa8b7c671e03ca428c2730e1f76ba84759c
Jackson <jackson@stemn.com>
2019-03-03 12:40:49 +0000

    working

--- .config/scripts/clean-secrets.sh
+++ .config/scripts/clean-secrets.sh
@@ -6,8 +6,13 @@ remove_vcsh_repo() {
   echo Cleaning $REPO...
 
   # Remove vcsh repository tracked files
-  vcsh $REPO ls-files --full-name $HOME | \
-    xargs -n 1 -I file rm -f $HOME/file
+  vcsh $REPO ls-files --full-name $HOME 2>/dev/null | \
+    xargs -n 1 -I file rm -rf $HOME/file 2>/dev/null
+
+  # Remove vcsh repository tracked directories
+  vcsh $REPO ls-files --full-name $HOME 2>/dev/null | \
+    xargs dirname | \
+    xargs -n 1 -I directory rm -rf $HOME/directory 2>/dev/null
 
   # Remove vcsh repository git folder
   # rm -rf ~/.config/vcsh/repo-private.d/$REPO.git 2>/dev/null
