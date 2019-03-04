ec8467c51a6e2c37103e1a561240172e84a72a1f
Jackson <jackson@stemn.com>
2019-03-04 00:25:07 +0000

    eval

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -18,7 +18,7 @@ if [ ! -f $HOME/.ssh/id_rsa ]; then
 fi
 
 # Unlock ssh private key so remaining repositories can be cloned
-keychain --eval id_rsa
+eval `keychain --eval id_rsa`
 
 # Clone private repositories using ssh key
 vcsh clone git@github.com:sabrehagen/dotfiles-cloudflare &
