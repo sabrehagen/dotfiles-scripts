6cac23d05b68599e4a889c4043aa677bdf1bede9
Jackson <jackson@stemn.com>
2019-03-13 08:31:33 +0000

    build

---
+++ .config/scripts/build-desktop.sh
@@ -0,0 +1,5 @@
+# Build stemn devlopment-environment
+yarn --cwd $HOME/repositories/stemn/stemn-backend build:development-environment
+
+# Bulid desktop environment
+$HOME/repositories/sabrehagen/desktop-environment/scripts/docker/build.sh
