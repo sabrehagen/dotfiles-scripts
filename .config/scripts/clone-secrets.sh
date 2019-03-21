e26ba07904380d14ab7155086054c70e1e4d15b1
Jackson Delahunt <jackson@stemn.com>
2019-03-22 10:02:32 +1100

    github

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -27,6 +27,7 @@ tmux set-environment SSH_AUTH_SOCK $SSH_AUTH_SOCK
 vcshp clone git@github.com:sabrehagen/dotfiles-cloudflare 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-gcloud 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-gdrive 2>/dev/null &
+vcshp clone git@github.com:sabrehagen/dotfiles-github 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-notes 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-npm 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-onedrive 2>/dev/null &
