cd4c40b3f5a3a6d3df74b29c47b72802f2479cc9
Jackson Delahunt <jackson@stemn.com>
2019-07-10 16:28:55 +1000

    sxhkd

--- .config/scripts/startup.sh
+++ .config/scripts/startup.sh
@@ -14,16 +14,9 @@ numlockx on
 # Set keyboard repeat delay and rate
 xset r rate 180 140
 
-# Manually start services whilst s6 issue persists
+# Start the tmux server and long lived services
 tmux start-server
-tmux new-session -d -s desktop-environment-dashboard zsh 2>/dev/null
 tmux new-session -d -s desktop-environment-shell zsh --login 2>/dev/null
-tmux new-session -d -s gotty-clients zsh 2>/dev/null
-tmux new-session -d -s gotty-server \
-  gotty \
-  --permit-write \
-  --port 8022 \
-  zsh -c 'tmux new-session -s gotty-clients-$(date +%s) -t gotty-clients' 2>/dev/null
 tmux new-session -d -s transmission \
   transmission-daemon \
   --bind-address-ipv4 localhost \
@@ -33,17 +26,4 @@ tmux new-session -d -s transmission \
   --no-auth \
   --rpc-bind-address localhost \
   --watch-dir $HOME/torrents/.watch 2>/dev/null
-
-# Always restart webrelay to pick up new environment variables
-tmux set-environment -g RELAY_KEY $RELAY_KEY
-tmux set-environment -g RELAY_SECRET $RELAY_SECRET
-tmux kill-session -t webrelay 2>/dev/null
-tmux new-session -d -s webrelay \
-  relay connect \
-  --crypto full \
-  --region au \
-  --subdomain $HOSTNAME \
-  http://localhost:8022 2>/dev/null
-
-# Insert the stemn docker host into the hosts file
-yarn --cwd $HOME/repositories/stemn/stemn-backend hosts:insert 2>/dev/null &
+tmux new-session -d -s sxhkd sxhkd
