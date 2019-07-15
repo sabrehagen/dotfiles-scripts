f447f2c6eb65f9611119f908773c0071243d8363
Jackson Delahunt <jackson@stemn.com>
2019-07-15 12:18:20 +1000

    clean

--- .config/scripts/build-desktop.sh
+++
@@ -1,5 +0,0 @@
-# Build stemn devlopment-environment
-yarn --cwd $HOME/repositories/stemn/stemn-backend build:development-environment
-
-# Bulid desktop environment
-$HOME/repositories/sabrehagen/desktop-environment/docker/scripts/build.sh
