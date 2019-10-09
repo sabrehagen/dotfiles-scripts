ce3cda9c2036e97a7c994d998d89711107756130
Jackson Delahunt <jackson@stemn.com>
2019-10-09 16:57:52 +1100

    brightness

---
+++ .config/scripts/brightness.sh
@@ -0,0 +1,15 @@
+BRIGHTNESS=`xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' '`
+
+if [ "$1" = '+' ]; then
+  NEW_BRIGHTNESS=$(echo "$BRIGHTNESS + 0.05" | bc)
+  if [ "$(echo "$NEW_BRIGHTNESS > 1.0" | bc)" -eq 1 ]; then
+    NEW_BRIGHTNESS='1.0'
+  fi
+elif [ "$1" = '-' ]; then
+  NEW_BRIGHTNESS=$(echo "$BRIGHTNESS - 0.05" | bc)
+  if [ "$(echo "$NEW_BRIGHTNESS < 0.0" | bc)" -eq 1 ]; then
+    NEW_BRIGHTNESS='0.0'
+  fi
+fi
+
+xrandr --output eDP-1 --brightness "$NEW_BRIGHTNESS"
