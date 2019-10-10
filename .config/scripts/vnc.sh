c43e59f494955346a7cc38aba46dbd61297e3fee
Jackson Delahunt <jackson@stemn.com>
2019-10-10 13:49:44 +1100

    vnc

---
+++ .config/scripts/vnc.sh
@@ -0,0 +1,13 @@
+# Start vnc server
+tmux new-session \
+  -d \
+  -s vnc-server \
+  x11vnc -forever \
+  2>/dev/null
+
+# Start vnc client
+tmux new-session \
+  -d \
+  -s vnc-client \
+  sudo /opt/noVNC/utils/launch.sh --listen 80 \
+  2>/dev/null
