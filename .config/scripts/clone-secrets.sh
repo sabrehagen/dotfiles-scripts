cc1d51a303143057666a7b42384be35484d31540
Jackson Delahunt <jackson@stemn.com>
2019-10-12 17:14:13 +1100

    echo

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -7,11 +7,13 @@ PRIVATE_VCSH_REPOS=$HOME/.config/vcsh/repo-private.d
 
 # Clone private ssh keys using password over https if an ssh key is not already present
 if [ ! -f $HOME/.ssh/id_rsa ]; then
-  echo; read -s "PASSWORD?Password for 'https://sabrehagen@github.com': "
+  echo
+  read -s "PASSWORD?Password for 'https://sabrehagen@github.com': "
   vcshp clone https://sabrehagen:$PASSWORD@github.com/sabrehagen/dotfiles-ssh-private && \
     chmod 600 $HOME/.ssh-private/id_rsa && \
     ln -sf $HOME/.ssh/id_rsa.pub $HOME/.ssh-private/id_rsa.pub && \
     ln -sf $HOME/.ssh-private/id_rsa $HOME/.ssh/id_rsa
+  echo
 fi
 
 # Ensure ssh-agent is started so keys can be loaded
