56f4f63a4b08ff83e9b05b3c9bfd27d18f0f131d
Jackson Delahunt <jackson@stemn.com>
2019-10-20 13:31:02 +1100

    +x

--- .config/scripts/vnc.sh
+++ .config/scripts/vnc.sh
@@ -2,12 +2,12 @@
 tmux new-session \
   -d \
   -s vnc-server \
-  x11vnc -forever -noxdamage \
+  x0vncserver -SecurityTypes none \
   2>/dev/null
 
 # Start vnc client
 tmux new-session \
   -d \
   -s vnc-client \
-  sudo /opt/noVNC/utils/launch.sh --listen 80 \
+  sudo /opt/noVNC/utils/launch.sh --listen 80 --vnc localhost:5901 \
   2>/dev/null
