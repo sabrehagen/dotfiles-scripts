a6111b67c7e6df7f0464fa4d11933ee518229ad8
Jackson Delahunt <jackson@stemn.com>
2020-08-29 22:24:02 +1000

    kill

--- .config/scripts/tmux-desktop-environment-build-dotfiles.sh
+++ .config/scripts/tmux-desktop-environment-build-dotfiles.sh
@@ -1,3 +1,10 @@
+# Kill any previous dotfiles builds
+tmux ls | \
+  grep build-dotfiles | \
+  cut -f1 -d: | \
+  xargs -n1 tmux kill-session -t \
+  2>/dev/null
+
 # Start desktop environment build dotfiles
 tmux new-session \
   -d \
