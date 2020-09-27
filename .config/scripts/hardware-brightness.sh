ae729a93bfa10cd2ea2dd94c2faf1a86c171ceaf
Jackson Delahunt <jackson@stemn.com>
2020-09-28 08:18:23 +1000

    brightness

--- .config/scripts/hardware-brightness.sh
+++ .config/scripts/hardware-brightness.sh
@@ -1,9 +1,9 @@
 BRIGHTNESS=$(~/.config/scripts/ssh-host.sh sudo cat /sys/class/backlight/intel_backlight/actual_brightness)
 
 if [ "$1" = '+' ]; then
-  NEW_BRIGHTNESS=$(($BRIGHTNESS + 1000))
+  NEW_BRIGHTNESS=$(($BRIGHTNESS + 10000))
 elif [ "$1" = '-' ]; then
-  NEW_BRIGHTNESS=$(($BRIGHTNESS - 1000))
+  NEW_BRIGHTNESS=$(($BRIGHTNESS - 10000))
 fi
 
 ~/.config/scripts/ssh-host.sh "echo $NEW_BRIGHTNESS | sudo tee /sys/class/backlight/intel_backlight/brightness"
