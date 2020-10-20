a800ac4f141303506341109f39ad1645e452da7a
Jackson Delahunt <jackson@stemn.com>
2020-10-21 01:27:00 +1100

    debugging

--- .config/scripts/git-fzf-log.sh
+++ .config/scripts/git-fzf-log.sh
@@ -7,7 +7,7 @@ git-log ()
    'f() { set -- $(echo -- "$@" | grep -o "[a-f0-9]\{7\}"); [ $# -eq 0 ] || git show --color=always $1 ; }; f {}' \
    --bind "j:down,k:up,alt-j:preview-down,alt-k:preview-up,ctrl-f:preview-page-down,ctrl-b:preview-page-up,q:abort,ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                xargs -I % sh -c 'git show --color=always % $1 | less -R') << 'FZF-EOF'
                 {}
 FZF-EOF" --preview-window=right:60%
 }
