e32f272c0be033db8daf2ca199f7ab4cd4046180
Ben Barnes <benjaminjgbarnes@gmail.com>
2020-11-08 15:31:04 +1100

    git config user.username

--- .config/scripts/host-desktop-environment-restart.sh
+++ .config/scripts/host-desktop-environment-restart.sh
@@ -1,2 +1,5 @@
+# get git username
+GIT_USERNAME=$(git config user.username)
+
 # Restart desktop environment on the host
-~/.config/scripts/ssh-host.sh tmux new-session -d /home/$USER/repositories/sabrehagen/desktop-environment/docker/scripts/restart.sh
+~/.config/scripts/ssh-host.sh tmux new-session -d /home/$USER/repositories/$GIT_USERNAME/desktop-environment/docker/scripts/restart.sh
