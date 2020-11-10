ed0ccfd2f610eaaaab603e611e65e38c4d85117d
Ben Barnes <benjaminjgbarnes@gmail.com>
2020-11-11 00:41:18 +1100

    godot

---
+++ .config/scripts/install/godot.sh
@@ -0,0 +1,9 @@
+sudo mkdir /usr/local/bin/godot
+
+sudo wget -O /usr/local/bin/godot -q https://downloads.tuxfamily.org/godotengine/3.2.3/Godot_v3.2.3-stable_x11.64.zip
+
+unzip /usr/local/bin/godot/Godot_v3.2.3-stable_x11.64.zip
+
+rm -rf Godot_v3.2.3-stable_x11.64.zip
+
+/usr/local/bin/godot/Godot_v3.2.3-stable_x11.64.zip
