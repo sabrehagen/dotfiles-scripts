1bb2f07767edc09456d4d1a629529cf7202799e4
Jackson Delahunt <jackson@stemn.com>
2020-11-12 17:14:07 +1100

    clean-secrets

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -23,14 +23,11 @@ vcshp clone git@github.com:sabrehagen/dotfiles-aws 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-cloudstorage 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-docker 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-gcloud 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-gdrive 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-gist 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-irssi 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-npm 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-onedrive 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-openvpn 2>/dev/null &
 vcshp clone git@github.com:sabrehagen/dotfiles-rescuetime 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-wtf 2>/dev/null &
 
 # Wait for repositories to clone in parallel
 wait
