819482811bcba2064f72d4956442c65869276eb2
Ben Barnes <benjaminjgbarnes@gmail.com>
2020-11-12 13:13:09 +1100

    recovery...

--- .config/scripts/upgrade.sh
+++ .config/scripts/upgrade.sh
@@ -1,11 +1,20 @@
+installerDir="$(dirname $(readlink -f $0))/install/"
+
+#array=(*)
+#echo "${array[2]}"
+
 # check if upgrade arg provided
 if [ -z $1 ]
 then
   echo "no upgrade specified"
   exit
+elif [ $1 = "-o" ]
+then
+  for f in $installerDir*; do echo $(basename $f | sed -r 's/(.+?).sh/\1/');
+  done | column
+  exit
 fi
 
-installerDir=$(dirname $(readlink -f $0))/install/
 
 toInstall=$(${installerDir}$1)
 
