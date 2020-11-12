35e3d06d9bc280d986d850b8f9bcc62e7849391a
Ben Barnes <benjaminjgbarnes@gmail.com>
2020-11-12 18:02:52 +1100

    secrets setup

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -1,6 +1,8 @@
 # Ensure secrets are clean before cloning
 $HOME/.config/scripts/clean-secrets.sh
 
+github_user=$(git config --get user.username)
+
 # Store private dotfiles separately from public ones
 PUBLIC_VCSH_REPOS=$HOME/.config/vcsh/repo.d
 PRIVATE_VCSH_REPOS=$HOME/.config/vcsh/repo-private.d
@@ -8,8 +10,8 @@ PRIVATE_VCSH_REPOS=$HOME/.config/vcsh/repo-private.d
 # Clone private ssh keys using password over https if an ssh key is not already present
 if [ ! -f $HOME/.ssh/id_rsa ]; then
   echo
-  read -s "PASSWORD?Password for 'https://sabrehagen@github.com': "
-  vcshp clone https://sabrehagen:$PASSWORD@github.com/sabrehagen/dotfiles-ssh-private && \
+  read -s "PASSWORD?Password for 'https://${github_user}@github.com': "
+  vcshp clone https://${github_user}:$PASSWORD@github.com/${github_user}/dotfiles-ssh-private && \
     chmod 600 $HOME/.ssh-private/id_rsa && \
     ln -sf $HOME/.ssh/id_rsa.pub $HOME/.ssh-private/id_rsa.pub && \
     ln -sf $HOME/.ssh-private/id_rsa $HOME/.ssh/id_rsa
@@ -19,18 +21,18 @@ fi
 eval $(keychain --eval id_rsa --inherit any)
 
 # Clone private repositories using ssh key
-vcshp clone git@github.com:sabrehagen/dotfiles-aws 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-cloudstorage 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-docker 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-gcloud 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-gdrive 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-gist 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-irssi 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-npm 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-onedrive 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-openvpn 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-rescuetime 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-wtf 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-aws 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-cloudstorage 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-docker 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-gcloud 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-gdrive 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-gist 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-irssi 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-npm 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-onedrive 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-openvpn 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-rescuetime 2>/dev/null &
+# vcshp clone git@github.com:sabrehagen/dotfiles-wtf 2>/dev/null &
 
 # Wait for repositories to clone in parallel
 wait
