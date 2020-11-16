e809b12915b8f72b234afa5f9d971d2535a854c2
Jackson Delahunt <jackson@stemn.com>
2020-11-17 00:36:05 +1100

    no-any

--- .config/scripts/launch-alacritty.sh
+++ .config/scripts/launch-alacritty.sh
@@ -2,7 +2,7 @@
 nohup alacritty "$@" >/dev/null 2>&1 &
 
 # Wait for alacritty window to appear
-xdotool search --any --pid $! --sync
+xdotool search --pid $! --sync
 
 # Apply terminal colours
 wal -Retn
