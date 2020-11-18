c79cf1c82d94ef027a9f849faf9d95c3340691aa
Jackson Delahunt <jackson@stemn.com>
2020-11-19 07:41:04 +1100

    no-tmux-ls

--- .config/scripts/desktop-environment-build-dotfiles-trigger.sh
+++ .config/scripts/desktop-environment-build-dotfiles-trigger.sh
@@ -1,7 +1,6 @@
 DESKTOP_ENVIRONMENT_LAST_MODIFIED=$(cat ~/repositories/sabrehagen/desktop-environment/.dotfiles-cachebust 2>/dev/null || echo 0)
 DOTFILES_LAST_MODIFIED=$(vcsh foreach log -1 --date=unix --format=%cd | grep -v dotfiles | sort | tail -n 1)
 
-# if [ "$DOTFILES_LAST_MODIFIED" -le "$DESKTOP_ENVIRONMENT_LAST_MODIFIED" ] && tmux ls | grep -q build-dotfiles; then
 if [ "$DOTFILES_LAST_MODIFIED" -ge "$DESKTOP_ENVIRONMENT_LAST_MODIFIED" ]; then
   ~/.config/scripts/tmux-desktop-environment-build-dotfiles.sh
 fi
