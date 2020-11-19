043164cfa049a331a059c536ee66365b12d6272f
Ben Barnes <benjaminjgbarnes@gmail.com>
2020-11-20 10:33:18 +1100

    set res & apply

--- .config/scripts/set-resolution.sh
+++ .config/scripts/set-resolution.sh
@@ -9,3 +9,6 @@ xrandr --newmode "1920x1080_60.02" 173.00 1920 2048 2248 2576  1080 1083 1088 11
 
 # Add mode
 xrandr --addmode Virtual1 1920x1080_60.02
+
+# Apply mode
+xrandr --output Virtual1 --primary --mode 1920x1080_60.02
