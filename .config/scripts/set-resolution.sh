6fc9232eb3d414f852f31d71a459a1387e29ad5d
Ben Barnes <benjaminjgbarnes@gmail.com>
2020-11-19 17:36:44 +1100

    set-res

---
+++ .config/scripts/set-resolution.sh
@@ -0,0 +1,11 @@
+# to add new mode run:
+# cvt width height refreshrate
+# this will output Modeline
+# insert modeline values into 2 commands below
+
+
+# create mode
+xrandr --newmode "1920x1080_60.02" 173.00 1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+
+# Add mode
+xrandr --addmode Virtual1 1920x1080_60.02
