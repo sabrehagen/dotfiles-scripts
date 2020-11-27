8d9d53439df4ec02653b5385de42c97b3cbf2c11
Ben Barnes <benjaminjgbarnes@gmail.com>
2020-11-27 11:19:29 +1100

    tidy set res output

--- .config/scripts/set-resolution.sh
+++ .config/scripts/set-resolution.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/zsh
 
 # to add new mode run:
 # cvt width height refreshrate
@@ -6,10 +6,16 @@
 # insert modeline values into 2 commands below
 
 
-function set_res(){
-
+function set_res () {
 # create mode
-xrandr --newmode "1920x1080_60.00" 173.00 1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+TRY_ADD_MODE=$(xrandr --newmode "1920x1080_60.00" 173.00 1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync 2>&1)
+
+if [ -z "$TRY_ADD_MODE" ]; then
+    echo "setting resolution"
+else
+    echo "resolution already set"
+    exit
+fi
 
 # Add mode
 xrandr --addmode Virtual1 1920x1080_60.00
@@ -18,7 +24,7 @@ xrandr --addmode Virtual1 1920x1080_60.00
 xrandr --output Virtual1 --primary --mode 1920x1080_60.00
 
 }
-
+echo oi
 # If vmware machine
 if [ -c /dev/vsock ]; then
     echo "you are using a vmware machine u little fucker"
@@ -29,6 +35,3 @@ elif [ -c /dev/vhost-vsock ]; then
 else
     echo "you are using a host machine u braggart"
 fi
-
-
-
