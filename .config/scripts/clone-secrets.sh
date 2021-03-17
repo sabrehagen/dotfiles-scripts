a22e8334cfc352c04841887d0f7522f9ca02d64c
Jackson Delahunt <jackson.delahunt@lendi.com.au>
2021-03-18 04:32:30 +1100

    dunst

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -1,40 +1,41 @@
 # Ensure secrets are clean before cloning
 $HOME/.config/scripts/clean-secrets.sh
 
-# Store private dotfiles separately from public ones
-PUBLIC_VCSH_REPOS=$HOME/.config/vcsh/repo.d
-PRIVATE_VCSH_REPOS=$HOME/.config/vcsh/repo-private.d
-
 # Clone private ssh keys using password over https if an ssh key is not already present
 if [ ! -f $HOME/.ssh/id_rsa ]; then
-  echo
-  read -s "PASSWORD?Password for 'https://sabrehagen@github.com': "
-  vcshp clone https://sabrehagen:$PASSWORD@github.com/sabrehagen/dotfiles-ssh-private && \
-    chmod 600 $HOME/.ssh-private/id_rsa && \
-    ln -sf $HOME/.ssh/id_rsa.pub $HOME/.ssh-private/id_rsa.pub && \
-    ln -sf $HOME/.ssh-private/id_rsa $HOME/.ssh/id_rsa
+  if [ -z $DESKTOP_ENVIRONMENT_GITHUB_TOKEN ]; then
+    echo
+    echo Login requires DESKTOP_ENVIRONMENT_GITHUB_TOKEN environment variable to be set!
+    return
+  fi
+
+  vcsh clone https://sabrehagen:$DESKTOP_ENVIRONMENT_GITHUB_TOKEN@github.com/sabrehagen/dotfiles-ssh-private && \
+    chmod 600 $HOME/.ssh-private/*
 fi
 
 # Unlock ssh private key so remaining repositories can be cloned
-eval $(keychain --eval id_rsa --inherit any)
+eval $(find ~/.ssh-private -regextype posix-extended -regex '.*id_rsa[a-z_]*' | xargs -n1 -I@ keychain --inherit any --eval @)
 
 # Clone private repositories using ssh key
-vcshp clone git@github.com:sabrehagen/dotfiles-aws 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-cloudstorage 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-docker 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-gcloud 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-gist 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-irssi 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-npm 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-openvpn 2>/dev/null &
-vcshp clone git@github.com:sabrehagen/dotfiles-rescuetime 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-aws 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-cloudstorage 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-docker 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-gauth 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-gcloud 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-gist 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-irssi 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-nchat 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-nmail 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-npm 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-openvpn 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-rescuetime 2>/dev/null &
 
 # Wait for repositories to clone in parallel
 wait
 
 # Convert all https cloned repositories to use ssh
 https_to_git () { sed -i 's;=.*://.*github.com/\(.*\);= git@github.com:\1;' "$1"; }
-for REPOSITORY in $(ls -d $PUBLIC_VCSH_REPOS/* $PRIVATE_VCSH_REPOS/*); do
+for REPOSITORY in $(ls -d ~/.config/vcsh/repo.d/*); do
   https_to_git $REPOSITORY/config
 done
 
