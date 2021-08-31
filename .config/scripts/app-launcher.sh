80b68e9fa7bf71dff132961d234625051a47b985
Jackson Delahunt <jackson@stemn.com>
2021-08-31 12:49:34 +1000

    launch-app

--- .config/scripts/app-launcher.sh
+++ .config/scripts/app-launcher.sh
@@ -1,21 +1,32 @@
 APPS_DIR=~/apps
+LAUNCH_APP=$1
 
-# Configure dmenu
-DMENU_FG=$(cat ~/.cache/wal/colors | head -1)
-DMENU_BG=$(cat ~/.cache/wal/colors | tail -1)
-DMENU_COMMAND="dmenu -nb $DMENU_FG -nf $DMENU_BG -sb $DMENU_BG -sf $DMENU_FG"
+# If an application was supplied
+if [ ! -z "$LAUNCH_APP" ]; then
 
-# Get application paths
-APP_PATHS=$(find $APPS_DIR/*/* -maxdepth 0 ! -path . -type d | sed -E "s;$APPS_DIR/;;" | sed 's;de-;;')
+  # Launch the supplied application
+  find $APPS_DIR/*/* -maxdepth 1 -type f -name $LAUNCH_APP -exec {} \; &
 
-# Present application names for user selection
-APP_NAME=$(echo $APP_PATHS | tr ' ' \\n | $DMENU_COMMAND)
+else
 
-# Restore application path from name
-APP_PATH=$(echo $APP_NAME | sed -E 's;([^/]*/);\1de-;')
+  # Configure dmenu
+  DMENU_FG=$(cat ~/.cache/wal/colors | head -1)
+  DMENU_BG=$(cat ~/.cache/wal/colors | tail -1)
+  DMENU_COMMAND="dmenu -nb $DMENU_FG -nf $DMENU_BG -sb $DMENU_BG -sf $DMENU_FG"
 
-# Get the application binary name
-APP_BINARY=$(echo $APP_PATH | sed 's;.*/;;')
+  # Get application paths
+  APP_PATHS=$(find $APPS_DIR/*/* -maxdepth 0 ! -path . -type d | sed -E "s;$APPS_DIR/;;" | sed 's;de-;;')
 
-# Run selected application
-$APPS_DIR/$APP_PATH/$APP_BINARY &
+  # Present application names for user selection
+  APP_NAME=$(echo $APP_PATHS | tr ' ' \\n | $DMENU_COMMAND)
+
+  # Restore application path from name
+  APP_PATH=$(echo $APP_NAME | sed -E 's;([^/]*/);\1de-;')
+
+  # Get the application binary name
+  APP_BINARY=$(echo $APP_PATH | sed 's;.*/;;')
+
+  # Run selected application
+  $APPS_DIR/$APP_PATH/$APP_BINARY &
+
+fi
