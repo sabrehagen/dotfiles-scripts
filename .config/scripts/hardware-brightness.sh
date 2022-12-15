ea15aee05bee4606090588852fddd032e6429c0e
Jackson Delahunt <jackson@stemn.com>
2022-12-16 03:18:29 +1100

    alter-hardware-brightness

--- .config/scripts/hardware-brightness.sh
+++ .config/scripts/hardware-brightness.sh
@@ -1,9 +1,10 @@
 BRIGHTNESS=$(~/.config/scripts/ssh-host.sh sudo cat /sys/class/backlight/intel_backlight/actual_brightness)
+INCREMENT=${2-1000}
 
 if [ "$1" = '+' ]; then
-  NEW_BRIGHTNESS=$(($BRIGHTNESS + 1000))
+  NEW_BRIGHTNESS=$(($BRIGHTNESS + $INCREMENT))
 elif [ "$1" = '-' ]; then
-  NEW_BRIGHTNESS=$(($BRIGHTNESS - 1000))
+  NEW_BRIGHTNESS=$(($BRIGHTNESS - $INCREMENT))
 fi
 
 ~/.config/scripts/ssh-host.sh "echo $NEW_BRIGHTNESS | sudo tee /sys/class/backlight/intel_backlight/brightness"
