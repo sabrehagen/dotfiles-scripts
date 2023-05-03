5c53afa4b88bd15724f45d79e6ba81b317cf46f8
Jackson Delahunt <jackson@stemn.com>
2023-05-03 21:47:24 +1000

    add-vs-code

--- .config/scripts/wal.sh
+++ .config/scripts/wal.sh
@@ -2,13 +2,22 @@
 ~/.local/bin/wal -i ~/.local/share/wallpapers
 
 # Reload dunst theme
-~/.config/dunst/wal.sh
+~/.config/dunst/wal.sh &
 
 # Reload xava
-kill -USR1 $(pgrep xava)
+kill -USR1 $(pgrep xava) &
 
 # Generate gtk theme
-/opt/warnai/warnai --wal --gtk fantome --norender
+/opt/warnai/warnai --wal --gtk fantome --norender &
 
 # Reload gtk theme
-gtk-theme-switch2 ~/.themes/warna
+gtk-theme-switch2 ~/.themes/warna &
+
+# Reload vs code theme
+xdotool search --onlyvisible --class code \
+  windowactivate --sync \
+  windowfocus --sync \
+  key ctrl+shift+r &
+
+# Wait for themes to update in parallel
+wait
