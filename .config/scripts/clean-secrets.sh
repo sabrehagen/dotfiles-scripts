55dbc532da754a46e369776e9db96dd8bac541af
Jackson Delahunt <jackson@stemn.com>
2023-08-26 12:38:19 +1000

    redundant-syntax

--- .config/scripts/clean-secrets.sh
+++ .config/scripts/clean-secrets.sh
@@ -5,12 +5,12 @@ remove_vcsh_repo() {
 
   # Remove vcsh repository tracked files
   vcsh $REPO ls-files --full-name $HOME 2>/dev/null | \
-    xargs -n 1 -I file rm -rf $HOME/file 2>/dev/null
+    xargs -I file rm -rf $HOME/file 2>/dev/null
 
   # Remove vcsh repository tracked directories
   vcsh $REPO ls-files --full-name $HOME 2>/dev/null | \
     xargs dirname 2>/dev/null | \
-    xargs -n 1 -I directory rm -rf $HOME/directory 2>/dev/null
+    xargs -I directory rm -rf $HOME/directory 2>/dev/null
 
   # Remove vcsh repository git folder
   rm -rf ~/.config/vcsh/repo.d/$REPO.git 2>/dev/null
