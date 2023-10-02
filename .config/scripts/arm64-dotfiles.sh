dc9936149dd4ea1352992150410373f55b5a5b78
Jackson Delahunt <jackson@stemn.com>
2023-10-03 03:30:01 +1100

    arm-noninteractive

--- .config/scripts/arm64-dotfiles.sh
+++ .config/scripts/arm64-dotfiles.sh
@@ -1,10 +1,6 @@
 ARCH=$(dpkg --print-architecture)
 
 if [ "$ARCH" = "arm64" ]; then
-  # handle alacritty
   vcsh dotfiles-alacritty checkout arm64
-
-  # handle i3
   vcsh dotfiles-i3 checkout arm64
-  i3-msg restart
 fi
