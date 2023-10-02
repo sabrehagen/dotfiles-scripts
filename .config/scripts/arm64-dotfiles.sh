5689c0adab3b4fe90a1789674e7b91588e1e3573
Jackson Delahunt <jackson@stemn.com>
2023-10-03 03:34:36 +1100

    ls-branches

--- .config/scripts/arm64-dotfiles.sh
+++ .config/scripts/arm64-dotfiles.sh
@@ -1,6 +1,7 @@
 ARCH=$(dpkg --print-architecture)
 
 if [ "$ARCH" = "arm64" ]; then
+  vcsh dotfiles-alacritty branch -a
   vcsh dotfiles-alacritty checkout arm64
   vcsh dotfiles-i3 checkout arm64
 fi
