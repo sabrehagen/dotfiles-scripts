b73c033f0c6a2d15eb9cb3d8d02cb9e3a031b4fb
Jackson Delahunt <jackson@stemn.com>
2023-10-03 03:37:34 +1100

    arm-before-init

--- .config/scripts/arm64-dotfiles.sh
+++ .config/scripts/arm64-dotfiles.sh
@@ -1,7 +1,6 @@
 ARCH=$(dpkg --print-architecture)
 
 if [ "$ARCH" = "arm64" ]; then
-  vcsh dotfiles-alacritty branch -a
   vcsh dotfiles-alacritty checkout arm64
   vcsh dotfiles-i3 checkout arm64
 fi
