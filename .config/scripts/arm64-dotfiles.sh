ef77f7f9c5e0d65f062fac68c2527e1d832f005a
Jackson Delahunt <jackson@stemn.com>
2023-10-04 12:01:47 +1100

    alacritty

--- .config/scripts/arm64-dotfiles.sh
+++ .config/scripts/arm64-dotfiles.sh
@@ -3,6 +3,7 @@ ARCH=$(dpkg --print-architecture)
 fetch_and_checkout_arm64 () { vcsh $1 fetch; vcsh $1 checkout arm64; }
 
 if [ "$ARCH" = "arm64" ]; then
+  fetch_and_checkout_arm64 dotfiles-alacritty
   fetch_and_checkout_arm64 dotfiles-i3
   fetch_and_checkout_arm64 dotfiles-x11
 fi
