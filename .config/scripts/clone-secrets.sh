54d42b6f6c08041c9349d2c49ff36cdb766816c3
Jackson Delahunt <jackson@stemn.com>
2023-11-03 21:44:19 +1100

    gh

--- .config/scripts/clone-secrets.sh
+++ .config/scripts/clone-secrets.sh
@@ -24,6 +24,7 @@ done
 
 # Clone private repositories using ssh key
 vcsh clone git@github.com:sabrehagen/dotfiles-fastfetch 2>/dev/null &
+vcsh clone git@github.com:sabrehagen/dotfiles-gh 2>/dev/null &
 vcsh clone git@github.com:sabrehagen/dotfiles-mopidy 2>/dev/null &
 vcsh clone git@github.com:sabrehagen/dotfiles-op 2>/dev/null &
 vcsh clone git@github.com:sabrehagen/dotfiles-openvpn 2>/dev/null &
