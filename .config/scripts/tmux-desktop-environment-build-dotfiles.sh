f841acb4e8322d4b991cc0c0dbda570fdbe8007b
Jackson Delahunt <jackson@stemn.com>
2023-11-22 18:04:41 +1100

    build-log-gotop

--- .config/scripts/tmux-desktop-environment-build-dotfiles.sh
+++ .config/scripts/tmux-desktop-environment-build-dotfiles.sh
@@ -1,9 +1,15 @@
 # Kill any previous dotfiles builds
 $HOME/.config/scripts/tmux-kill-desktop-environment-build-dotfiles.sh
 
+SESSION_NAME=desktop-environment-build-dotfiles-$(date +%s)
+
 # Start desktop environment build dotfiles
 tmux new-session \
   -d \
-  -s desktop-environment-build-dotfiles-$(date +%s) \
-  $HOME/repositories/sabrehagen/desktop-environment/docker/scripts/build-dotfiles.sh \
+  -s $SESSION_NAME \
+  zsh -c "$HOME/repositories/sabrehagen/desktop-environment/docker/scripts/build-dotfiles.sh; sleep infinity" \
   2>/dev/null
+
+# Open a gotop host network monitor in the build session
+tmux split-window -t $SESSION_NAME $HOME/.config/scripts/host-network.sh
+tmux resize-pane -t ${SESSION_NAME}:1 -y 5
