68f48deb5389cb7eb0ce8c5c9f135b7ee66d8280
Jackson Delahunt <jackson@stemn.com>
2024-07-11 00:18:34 +1000

    colours-final

--- .config/scripts/git-diff-classic.sh
+++ .config/scripts/git-diff-classic.sh
@@ -1,13 +1,13 @@
 # Force classic red and green colours for git diff output
 git -c color.diff-highlight.oldNormal='203 bold' \
     -c color.diff-highlight.oldHighlight='203 bold 52' \
-    -c color.diff-highlight.newNormal='114 bold' \
-    -c color.diff-highlight.newHighlight='114 bold 22' \
-    -c color.diff.meta='227' \
-    -c color.diff.frag='92 bold' \
+    -c color.diff-highlight.newNormal='113 bold' \
+    -c color.diff-highlight.newHighlight='113 bold 22' \
+    -c color.diff.meta='226' \
+    -c color.diff.frag='135 bold' \
     -c color.diff.commit='yellow bold' \
     -c color.diff.old='203 bold' \
-    -c color.diff.new='114 bold' \
+    -c color.diff.new='113 bold' \
     -c color.diff.whitespace='203 reverse' \
-    -c color.diff.func='98 bold' \
+    -c color.diff.func='103 bold' \
     diff "$@"
