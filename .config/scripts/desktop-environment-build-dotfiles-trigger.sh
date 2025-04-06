a097f18a09c4f628db90cb2e4ca3a9ee2cc30f0a
Jackson Delahunt <jackson@stemn.com>
2025-04-07 09:44:16 +1000

    silence-errors

--- .config/scripts/desktop-environment-build-dotfiles-trigger.sh
+++ .config/scripts/desktop-environment-build-dotfiles-trigger.sh
@@ -1,5 +1,5 @@
 DESKTOP_ENVIRONMENT_LAST_MODIFIED=$(cat $HOME/repositories/sabrehagen/desktop-environment/.cachebust-dotfiles 2>/dev/null || echo 0)
-DOTFILES_LAST_MODIFIED=$(vcsh foreach log -1 --date=unix --format=%cd | grep -v dotfiles | sort | tail -n 1)
+DOTFILES_LAST_MODIFIED=$(vcsh foreach log -1 --date=unix --format=%cd 2>/dev/null | grep -v dotfiles | sort | tail -n 1)
 
 if [ "$DOTFILES_LAST_MODIFIED" -ge "$DESKTOP_ENVIRONMENT_LAST_MODIFIED" ]; then
   $HOME/.config/scripts/tmux-desktop-environment-build-dotfiles.sh
