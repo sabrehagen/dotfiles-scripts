47e4091de911dd803a5335121e006a4ea5f06406
Jackson Delahunt <jackson@stemn.com>
2026-01-01 17:03:21 +1100

    quote-ssh-script

--- .config/scripts/host-ssh-tty.sh
+++ .config/scripts/host-ssh-tty.sh
@@ -1,4 +1,4 @@
 PRIVATE_KEY_PATH=$HOME/.ssh/desktop-environment-host-access
 
 # Connect to host with tty and interactive shell environment
-$HOME/.config/scripts/host-ssh.sh -t zsh -i ${@:+-c "$@"}
+$HOME/.config/scripts/host-ssh.sh -t zsh -i ${@:+-c \""$@"\"}
