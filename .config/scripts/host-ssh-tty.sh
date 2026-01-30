6cdfbf7832030d2861a57a855a2ddf1ed19c9c83
Jackson Delahunt <jackson@stemn.com>
2026-01-31 02:50:14 +1100

    remove-redundant

--- .config/scripts/host-ssh-tty.sh
+++ .config/scripts/host-ssh-tty.sh
@@ -1,4 +1,2 @@
-PRIVATE_KEY_PATH=$HOME/.ssh/desktop-environment-host-access
-
 # Connect to host with tty and interactive shell environment
 $HOME/.config/scripts/host-ssh.sh -t zsh -i ${@:+-c \""$@"\"}
