31eba6387db35b45825ea0e017ceae78e2412d0d
Jackson Delahunt <jackson@stemn.com>
2026-01-31 16:57:32 +1100

    tag-gotop

--- .config/scripts/tmux-desktop-environment-build-dotfiles.sh
+++ .config/scripts/tmux-desktop-environment-build-dotfiles.sh
@@ -2,6 +2,7 @@
 $HOME/.config/scripts/tmux-kill-desktop-environment-build.sh
 
 SESSION_NAME=desktop-environment-build-dotfiles-$(date +%s)
+GOTOP_PROCESS=gotop-build-$(date +%s | tail -c 3)
 
 # Start desktop environment build dotfiles
 tmux new-session \
@@ -9,11 +10,11 @@ tmux new-session \
   -s $SESSION_NAME \
   zsh -c "\
     $HOME/repositories/sabrehagen/desktop-environment/docker/scripts/build-dotfiles.sh; \
-    $HOME/.config/scripts/host-ssh.sh pkill -STOP -x $SESSION_NAME; \
+    $HOME/.config/scripts/host-ssh.sh pkill -STOP -x $GOTOP_PROCESS; \
     sleep infinity \
   " \
   2>/dev/null
 
 # Open a gotop host network monitor in the build session
-tmux split-window -t $SESSION_NAME zsh -c "$HOME/.config/scripts/host-network.sh $SESSION_NAME; sleep infinity"
-tmux resize-pane -t $SESSION_NAME:1 -y 5
+tmux split-window -t $SESSION_NAME zsh -c "$HOME/.config/scripts/host-network.sh $GOTOP_PROCESS; sleep infinity"
+tmux resize-pane -t $SESSION_NAME:1 -y 6
