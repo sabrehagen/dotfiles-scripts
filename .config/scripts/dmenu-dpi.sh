a6ed15ab3a0b8e2a37dc05d6c94d5b4e0a9c7ed2
Jackson Delahunt <jackson@stemn.com>
2026-03-11 01:41:07 +1100

    dpi-set

--- .config/scripts/dmenu-dpi.sh
+++ .config/scripts/dmenu-dpi.sh
@@ -3,20 +3,5 @@ DPI_OPTIONS="96 144 192 254"
 NEW_DPI=$(echo $DPI_OPTIONS | tr ' ' \\n | $HOME/.config/scripts/dmenu.sh -p "Set DPI ($CURRENT_DPI):")
 
 if [ -n "$NEW_DPI" ]; then
-  # Update xresources and apply to x server
-  sed -iE "s/Xft.dpi.*/Xft.dpi: $NEW_DPI/" $HOME/.Xresources
-  xrdb -merge $HOME/.Xresources
-
-  # Set resolution based on DPI
-  if [ $NEW_DPI = "96" ]; then
-    xrandr --size 1920x1080
-  elif [ $NEW_DPI = "144" ]; then
-    xrandr --size 3840x2160
-  fi
-
-  # Update i3
-  i3-msg restart >/dev/null
-
-  # Update wallpaper window to match resolution
-  xdotool windowsize $(xdotool search --class xwinwrap) 100% 100%
+  $HOME/.config/scripts/dpi-set.sh $NEW_DPI
 fi
---
+++ .config/scripts/dpi-set.sh
@@ -0,0 +1,22 @@
+NEW_DPI=$1
+
+set -eu pipefail
+
+# Set resolution and i3blocks spacing based on DPI
+if [ $NEW_DPI = "96" ]; then
+  xrandr --size 1920x1080
+  sed -i -e 's/^separator_block_width=.*/separator_block_width=18/' $HOME/.config/i3blocks/config
+elif [ $NEW_DPI = "144" ]; then
+  xrandr --size 3840x2160
+  sed -i -e 's/^separator_block_width=.*/separator_block_width=24/' $HOME/.config/i3blocks/config
+fi
+
+# Update i3
+i3-msg restart >/dev/null
+
+# Update wallpaper window to match resolution
+xdotool windowsize $(xdotool search --class xwinwrap) 100% 100%
+
+# Update xresources and apply to x server
+sed -iE "s/Xft.dpi.*/Xft.dpi: $NEW_DPI/" $HOME/.Xresources
+xrdb -merge $HOME/.Xresources
