cbd1e24d2f18ff2ff34345f77c4f27cccbad79ec
Jackson Delahunt <jackson@stemn.com>
2026-03-13 01:01:17 +1100

    dpi

--- .config/scripts/dpi-set.sh
+++ .config/scripts/dpi-set.sh
@@ -1,10 +1,10 @@
-NEW_DPI=$1
+DPI=$1
 
 # Set resolution and i3blocks spacing based on DPI
-if [ $NEW_DPI = "96" ]; then
+if [ $DPI = "96" ]; then
   RESOLUTION=1920x1080
   I3_BLOCKS_SEPARATOR_WIDTH=18
-elif [ $NEW_DPI = "144" ]; then
+elif [ $DPI = "144" ]; then
   RESOLUTION=3840x2160
   I3_BLOCKS_SEPARATOR_WIDTH=22
 fi
@@ -16,7 +16,7 @@ xrandr --size $RESOLUTION || exit $?
 sed -i -e s/^separator_block_width=.*/separator_block_width=$I3_BLOCKS_SEPARATOR_WIDTH/ $HOME/.config/i3blocks/config
 
 # Update xresources and apply to x server
-sed -iE "s/Xft.dpi.*/Xft.dpi: $NEW_DPI/" $HOME/.Xresources
+sed -iE "s/Xft.dpi.*/Xft.dpi: $DPI/" $HOME/.Xresources
 xrdb -merge $HOME/.Xresources
 
 # Update i3 to use new dpi
