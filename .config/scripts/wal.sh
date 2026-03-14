6d625f678e008c8b3dfe948bbd48c32c581e1b24
Jackson Delahunt <jackson@stemn.com>
2026-03-14 16:24:36 +1100

    move-mpv

--- .config/scripts/wal.sh
+++ .config/scripts/wal.sh
@@ -7,6 +7,11 @@ gtk-theme-switch2 $HOME/.themes/warna
 # Update tty, reload i3, reload gtk
 wal -n -i ${1-$HOME/.local/share/wallpapers} --saturate 0.3
 
+# Update mpv wallpapr
+WALL_CACHE=$HOME/.cache/wal
+printf '{"command":["loadfile","%s","replace"]}\n' $(cat $WALL_CACHE/wal) \
+  | socat - $WALL_CACHE/mpv-ipc
+
 # Update telegram theme
 /opt/walogram/walogram
 
@@ -18,8 +23,3 @@ $HOME/.config/dunst/wal.sh
 
 # Reload xava
 kill -USR1 $(pgrep xava) 2>/dev/null
-
-# Update mpv wallpapr
-WALL_CACHE=$HOME/.cache/wal
-printf '{"command":["loadfile","%s","replace"]}\n' $(cat $WALL_CACHE/wal) \
-  | socat - $WALL_CACHE/mpv-ipc
