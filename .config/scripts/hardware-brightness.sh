7fd94a21e9bd4c40629651e159289229cce2071b
Jackson Delahunt <jackson@stemn.com>
2026-03-22 12:21:14 +1100

    ddc-brightness

--- .config/scripts/hardware-brightness.sh
+++ .config/scripts/hardware-brightness.sh
@@ -1,5 +1,18 @@
-BRIGHTNESS=$($HOME/.config/scripts/host-ssh.sh sudo cat /sys/class/backlight/intel_backlight/actual_brightness)
-INCREMENT=${2-1000}
+DDC_CACHE=$HOME/.ddcutil_cache
+INTEL_BACKLIGHT=/sys/class/backlight/intel_backlight
+
+set_brightness_intel() { $HOME/.config/scripts/host-ssh.sh "echo $1 | sudo tee $INTEL_BACKLIGHT/brightness"; }
+set_brightness_ddc() { flock $DDC_CACHE sudo ddcutil --bus 1 setvcp 10 $1 && echo $1 > $DDC_CACHE; }
+
+if $HOME/.config/scripts/host-ssh.sh test -d $INTEL_BACKLIGHT; then
+  BRIGHTNESS=$($HOME/.config/scripts/host-ssh.sh sudo cat $INTEL_BACKLIGHT/actual_brightness)
+  INCREMENT=${2-1000}
+  SET_BRIGHTNESS=set_brightness_intel
+else
+  BRIGHTNESS=$(cat $DDC_CACHE 2>/dev/null || sudo ddcutil --bus 1 getvcp --brief 10 2>/dev/null | awk '{print $4}')
+  INCREMENT=${2-10}
+  SET_BRIGHTNESS=set_brightness_ddc
+fi
 
 if [ "$1" = '+' ]; then
   NEW_BRIGHTNESS=$(($BRIGHTNESS + $INCREMENT))
@@ -7,4 +20,4 @@ elif [ "$1" = '-' ]; then
   NEW_BRIGHTNESS=$(($BRIGHTNESS - $INCREMENT))
 fi
 
-$HOME/.config/scripts/host-ssh.sh "echo $NEW_BRIGHTNESS | sudo tee /sys/class/backlight/intel_backlight/brightness"
+$SET_BRIGHTNESS $NEW_BRIGHTNESS
