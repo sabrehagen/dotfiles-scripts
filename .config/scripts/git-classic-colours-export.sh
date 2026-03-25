584d5dc2b8cf98a5de68e3a539966a726a0e39e2
Jackson Delahunt <jackson@stemn.com>
2026-03-25 14:59:36 +1100

    git-export

--- .config/scripts/git-classic-colours-export.sh
+++ .config/scripts/git-classic-colours-export.sh
@@ -1,12 +1,18 @@
-# Force classic red and green colours for git diff output for all git usage
-git config --global color.diff-highlight.oldNormal '203 bold'
-git config --global color.diff-highlight.oldHighlight '203 bold 52'
-git config --global color.diff-highlight.newNormal '113 bold'
-git config --global color.diff-highlight.newHighlight '113 bold 22'
-git config --global color.diff.meta '226'
-git config --global color.diff.frag '135 bold'
-git config --global color.diff.commit 'yellow bold'
-git config --global color.diff.old '203 bold'
-git config --global color.diff.new '113 bold'
-git config --global color.diff.whitespace '203 reverse'
-git config --global color.diff.func '146 bold'
+# Toggle classic red/green diff colours in the current shell environment.
+
+if [ -n "$GIT_CONFIG_PARAMETERS" ]; then
+  unset GIT_CONFIG_PARAMETERS
+else
+  export GIT_CONFIG_PARAMETERS="\
+'color.diff-highlight.oldNormal=203 bold' \
+'color.diff-highlight.oldHighlight=203 bold 52' \
+'color.diff-highlight.newNormal=113 bold' \
+'color.diff-highlight.newHighlight=113 bold 22' \
+'color.diff.meta=226' \
+'color.diff.frag=135 bold' \
+'color.diff.commit=yellow bold' \
+'color.diff.old=203 bold' \
+'color.diff.new=113 bold' \
+'color.diff.whitespace=203 reverse' \
+'color.diff.func=146 bold'"
+fi
